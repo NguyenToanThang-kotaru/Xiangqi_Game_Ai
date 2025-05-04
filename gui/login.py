4ea09e349f158c4c7c834518b9cfe96e97393f81
NguyenToanThang-kotaru <NguyenToanThang15122005@gmail.com>
2025-05-05 02:38:24 +0700

    chua duoc

--- gui/login.py
+++ gui/login.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) 
 
 from database.function_db.db_login import check_login
 from main_menu import openMenu
